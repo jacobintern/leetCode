28a607fdb02656480d3d7121a9e3f6993b0b6abe
jacob <sles4511@gmail.com>
2021-04-12 00:08:17 +0800

    Q4

--- roman-to-integer-easy/Q4_test.go
+++ roman-to-integer-easy/Q4_test.go
@@ -11,9 +11,48 @@ type parameters struct {
 
 func Test1(t *testing.T) {
 	params := parameters{
+		str: "MCMXCIV",
+	}
+	expected := 1994
+
+	if reflect.DeepEqual(expected, romanToInt(params.str)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
+
+func Test2(t *testing.T) {
+	params := parameters{
 		str: "III",
 	}
 	expected := 3
+
+	if reflect.DeepEqual(expected, romanToInt(params.str)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
+
+func Test3(t *testing.T) {
+	params := parameters{
+		str: "IV",
+	}
+	expected := 4
+
+	if reflect.DeepEqual(expected, romanToInt(params.str)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
+
+func Test4(t *testing.T) {
+	params := parameters{
+		str: "IX",
+	}
+	expected := 9
 
 	if reflect.DeepEqual(expected, romanToInt(params.str)) {
 		t.Log("sucess")
--- roman-to-integer-easy/main.go
+++ roman-to-integer-easy/main.go
@@ -1,5 +1,34 @@
 package Q4
 
+import (
+	"strings"
+)
+
+var numList map[string]int
+
+func init() {
+	numList = make(map[string]int)
+	numList["I"] = 1
+	numList["V"] = 5
+	numList["X"] = 10
+	numList["L"] = 50
+	numList["C"] = 100
+	numList["D"] = 500
+	numList["M"] = 1000
+	numList["IV"] = -2
+	numList["IX"] = -2
+	numList["XL"] = -20
+	numList["XC"] = -20
+	numList["CD"] = -200
+	numList["CM"] = -200
+}
+
 func romanToInt(s string) int {
-	return 1
+	var r int
+	for i, v := range numList {
+		if c := strings.Count(s, i); c > 0 {
+			r += c * v
+		}
+	}
+	return r
 }
