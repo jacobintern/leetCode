712f916b74b9bb543433d5aa9f846abb909ca5ec
jacob <sles4511@gmail.com>
2021-04-15 18:37:54 +0800

    Q7

---
+++ merge-two-sorted-lists/Q7_test.go
@@ -0,0 +1,25 @@
+package Q7
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	arr1 ListNode
+	arr2 ListNode
+}
+
+func Test1(t *testing.T) {
+	params := parameters{
+		arr1: ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}},
+		arr2: ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}},
+	}
+	expected := true
+
+	if reflect.DeepEqual(expected, mergeTwoLists(&params.arr1, &params.arr2)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
---
+++ merge-two-sorted-lists/mian.go
@@ -0,0 +1,11 @@
+package Q7
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var r ListNode
+	return &r
+}
