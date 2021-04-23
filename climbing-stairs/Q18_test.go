492b3fc3dbae8907714e158f13d41e84ea47ef8d
jacob <sles4511@gmail.com>
2021-04-23 16:32:18 +0800

    Q18

---
+++ climbing-stairs/Q18_test.go
@@ -0,0 +1,36 @@
+package Q18
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	num int
+}
+
+func Test1(t *testing.T) {
+	params := parameters{
+		num: 2,
+	}
+	expected := 2
+
+	if reflect.DeepEqual(expected, climbStairs(params.num)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
+
+func Test2(t *testing.T) {
+	params := parameters{
+		num: 3,
+	}
+	expected := 3
+
+	if reflect.DeepEqual(expected, climbStairs(params.num)) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail")
+	}
+}
