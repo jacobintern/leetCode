bbc2142879a36a7852712dfbfe9b1d576a3f91b6
jacob <sles4511@gmail.com>
2023-07-19 23:18:15 +0800

    change char to string

--- Q46_excel_sheet_column_title/main.go
+++ Q46_excel_sheet_column_title/main.go
@@ -1,39 +1,39 @@
 package Q47
 
-var list = map[int]byte{
-	1:  'A',
-	2:  'B',
-	3:  'C',
-	4:  'D',
-	5:  'E',
-	6:  'F',
-	7:  'G',
-	8:  'H',
-	9:  'I',
-	10: 'J',
-	11: 'K',
-	12: 'L',
-	13: 'M',
-	14: 'N',
-	15: 'O',
-	16: 'P',
-	17: 'Q',
-	18: 'R',
-	19: 'S',
-	20: 'T',
-	21: 'U',
-	22: 'V',
-	23: 'W',
-	24: 'X',
-	25: 'Y',
-	26: 'Z',
+var list = map[int]string{
+	1:  "A",
+	2:  "B",
+	3:  "C",
+	4:  "D",
+	5:  "E",
+	6:  "F",
+	7:  "G",
+	8:  "H",
+	9:  "I",
+	10: "J",
+	11: "K",
+	12: "L",
+	13: "M",
+	14: "N",
+	15: "O",
+	16: "P",
+	17: "Q",
+	18: "R",
+	19: "S",
+	20: "T",
+	21: "U",
+	22: "V",
+	23: "W",
+	24: "X",
+	25: "Y",
+	26: "Z",
 }
 
 func convertToTitle(columnNumber int) string {
-	res := []byte{}
+	res := ""
 	tmp := columnNumber
 	// 代表位置
-	for tmp > 0 {
+	for tmp > 26 {
 		v := tmp / 26
 
 		if v == 0 {
@@ -42,17 +42,18 @@ func convertToTitle(columnNumber int) string {
 
 		// 找出有幾個 26 就是那一個位數
 		if v > 26 {
-			res = append(res, list[v/26])
+			res += list[v/26]
 		} else {
-			res = append(res, list[v])
+			res += list[v]
 		}
+
 		tmp = ((v / 26) * 26) + v%26
 	}
 
 	b := columnNumber % 26
 	// 最後加上餘數
-	res = append(res, list[b])
-	return string(res)
+	res += list[b]
+	return res
 }
 
 func convertToTitle2(columnNumber int) string {
