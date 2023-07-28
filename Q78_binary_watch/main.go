79730db47ab47c6e79f7bf61df09ddfa56ff48b2
Jacoblin <sles4511@gmail.com>
2023-07-28 16:09:38 +0800

    q78 add another ans

--- Q78_binary_watch/main.go
+++ Q78_binary_watch/main.go
@@ -1,25 +1,44 @@
 package Q78
 
-import "fmt"
+import (
+	"fmt"
+	"math/bits"
+)
+
+func readBinaryWatch2(turnedOn int) []string {
+	if turnedOn == 0 {
+		return []string{"0:00"}
+	}
+	if turnedOn > 8 {
+		return []string{}
+	}
+
+	res := []string{}
+	// 先分配好數量再各自計算出結果，最後將結果組合起來
+	// H 的次數必須小於 4
+	// M 的次數必須小於 6
+	m := make(map[int]int)
+	for i := 3; i >= 0; i-- {
+		if i > turnedOn || turnedOn-i >= 6 {
+			continue
+		}
+		m[i] = turnedOn - i
+	}
+
+	return res
+}
 
 func readBinaryWatch(turnedOn int) []string {
 	res := []string{}
 	if turnedOn > 8 {
 		return res
 	}
-	// 二進制 1 的數量等於亮的燈數
-	list := make([]int, 60)
-
-	for i := 1; i < 60; i++ {
-		list[i] = list[i/2] + (i & 1)
-	}
-
 	// 取 H
 	for h := 0; h < 12; h++ {
 		m := 0
 		// 取 M
 		for m < 60 {
-			if list[h]+list[m] == turnedOn {
+			if bits.OnesCount(uint(h))+bits.OnesCount(uint(m)) == turnedOn {
 				res = append(res, fmt.Sprintf("%d:%02d", h, m))
 			}
 			m++
