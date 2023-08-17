a15ff87604af3c9f8470ad7c74a9053e6ceb1a9a
Jacoblin <sles4511@gmail.com>
2023-08-17 17:40:45 +0800

    add q102

---
+++ Q102_longest_uncommon_subsequence_i/Quest_test.go
@@ -0,0 +1,50 @@
+package Q102
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	str1 string
+	str2 string
+}
+
+func Test1(t *testing.T) {
+
+	params := parameters{str1: "aba", str2: "cdc"}
+
+	expected := 3
+
+	if testResult := findLUSlength(params.str1, params.str2); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test2(t *testing.T) {
+
+	params := parameters{str1: "aaa", str2: "bbb"}
+
+	expected := 3
+
+	if testResult := findLUSlength(params.str1, params.str2); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test3(t *testing.T) {
+
+	params := parameters{str1: "aaa", str2: "aaa"}
+
+	expected := -1
+
+	if testResult := findLUSlength(params.str1, params.str2); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
