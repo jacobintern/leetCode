846a85df4f381c3233b84095faa8d1c81968f053
Jacoblin <sles4511@gmail.com>
2023-08-29 11:31:58 +0800

    add Q107

---
+++ Q107_array_partition/Quest_test.go
@@ -0,0 +1,36 @@
+package Q107
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	nums []int
+}
+
+func Test1(t *testing.T) {
+
+	params := parameters{nums: []int{1, 4, 3, 2}}
+
+	expected := 4
+
+	if testResult := arrayPairSum(params.nums); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test2(t *testing.T) {
+
+	params := parameters{nums: []int{6, 2, 6, 5, 1, 2}}
+
+	expected := 9
+
+	if testResult := arrayPairSum(params.nums); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
