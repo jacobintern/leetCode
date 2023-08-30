c8b8c8ab01951994c62b23a7a891b48d857e3f99
Jacoblin <sles4511@gmail.com>
2023-08-30 15:57:53 +0800

    add Q110

---
+++ Q110_subtree_of_another_tree/Quest_test.go
@@ -0,0 +1,54 @@
+package Q110
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	root    TreeNode
+	subRoot TreeNode
+}
+
+func Test1(t *testing.T) {
+
+	params := parameters{
+		root: TreeNode{Val: 3,
+			Left: &TreeNode{Val: 4,
+				Left:  &TreeNode{Val: 1},
+				Right: &TreeNode{Val: 2}},
+			Right: &TreeNode{Val: 5}},
+		subRoot: TreeNode{Val: 4,
+			Left:  &TreeNode{Val: 1},
+			Right: &TreeNode{Val: 2}}}
+
+	expected := true
+
+	if testResult := isSubtree(&params.root, &params.subRoot); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test2(t *testing.T) {
+
+	params := parameters{
+		root: TreeNode{Val: 3,
+			Left: &TreeNode{Val: 4,
+				Left: &TreeNode{Val: 1},
+				Right: &TreeNode{Val: 2,
+					Left: &TreeNode{Val: 0}}},
+			Right: &TreeNode{Val: 5}},
+		subRoot: TreeNode{Val: 4,
+			Left:  &TreeNode{Val: 1},
+			Right: &TreeNode{Val: 2}}}
+
+	expected := false
+
+	if testResult := isSubtree(&params.root, &params.subRoot); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
