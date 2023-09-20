caa1c33e4881b68a09e5d32d906db915d2b294a5
Jacoblin <sles4511@gmail.com>
2023-09-20 13:30:47 +0800

    feat: add q113

---
+++ Q113_longest_harmonious_subsequence/Quest_test.go
@@ -0,0 +1,23 @@
+package Q113
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	arr []int
+}
+
+func Test1(t *testing.T) {
+
+	params := parameters{arr: []int{1, 3, 2, 2, 5, 2, 3, 7}}
+
+	expected := 5
+
+	if testResult := findLHS(params.arr); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
