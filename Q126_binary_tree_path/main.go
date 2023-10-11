33a12688131a1e63d7f4b217f04130a3e96da95b
Jacoblin <sles4511@gmail.com>
2023-10-11 13:34:38 +0800

    feat: add q126

--- Q126_binary_tree_path/main.go
+++ Q126_binary_tree_path/main.go
@@ -1,6 +1,8 @@
 package Q126
 
-import "strconv"
+import (
+	"strconv"
+)
 
 type TreeNode struct {
 	Val   int
@@ -8,30 +10,35 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+var res []string
+
 func binaryTreePaths(root *TreeNode) []string {
 	if root == nil {
 		return nil
 	}
-	path := strconv.Itoa(root.Val)
-	res := []string{}
-	traverslNode(root.Left, path, res)
-	traverslNode(root.Right, path, res)
+	res = []string{}
+	traverslNode(root, "")
 
 	return res
 }
 
-func traverslNode(root *TreeNode, path string, res []string) {
-	path = path + "->" + strconv.Itoa(root.Val)
-
-	if root.Left == nil && root.Right == nil {
-		res = append(res, path)
-		return
+func traverslNode(root *TreeNode, path string) string {
+	if path == "" {
+		path = strconv.Itoa(root.Val)
+	} else {
+		path = path + "->" + strconv.Itoa(root.Val)
 	}
 
 	if root.Left != nil {
-		traverslNode(root.Left, path, res)
+		traverslNode(root.Left, path)
 	}
 	if root.Right != nil {
-		traverslNode(root.Right, path, res)
+		traverslNode(root.Right, path)
 	}
+
+	if root.Left == nil && root.Right == nil {
+		res = append(res, path)
+	}
+
+	return path
 }
