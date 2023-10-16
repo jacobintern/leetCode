56eab8f292347b70c4a88e8f5bcdb7b7b18490dd
Jacoblin <sles4511@gmail.com>
2023-10-16 14:21:19 +0800

    feat:add q129

--- Q129_construct_string_from_binary_tree/Quest_test.go
+++ Q129_construct_string_from_binary_tree/Quest_test.go
@@ -16,7 +16,23 @@ func Test1(t *testing.T) {
 			Left: &TreeNode{Val: 4}},
 		Right: &TreeNode{Val: 3}}}
 
-	expected := 0
+	expected := "1(2(4))(3)"
+
+	if testResult := tree2str(params.root); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test2(t *testing.T) {
+
+	params := parameters{root: &TreeNode{Val: 1,
+		Left: &TreeNode{Val: 2,
+			Right: &TreeNode{Val: 4}},
+		Right: &TreeNode{Val: 3}}}
+
+	expected := "1(2()(4))(3)"
 
 	if testResult := tree2str(params.root); reflect.DeepEqual(expected, testResult) {
 		t.Log("sucess")
--- Q129_construct_string_from_binary_tree/main.go
+++ Q129_construct_string_from_binary_tree/main.go
@@ -1,5 +1,9 @@
 package Q129
 
+import (
+	"strconv"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,5 +11,22 @@ type TreeNode struct {
 }
 
 func tree2str(root *TreeNode) string {
-	return ""
+	if root == nil {
+		return ""
+	}
+
+	cur := strconv.Itoa(root.Val)
+
+	if root.Left == nil && root.Right == nil {
+		return cur
+	}
+
+	left := tree2str(root.Left)
+	right := tree2str(root.Right)
+	cur += "(" + left + ")"
+	if right != "" {
+		cur += "(" + right + ")"
+	}
+
+	return cur
 }
