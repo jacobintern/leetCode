c55b53b7781d35d8a2b733caa6d435985e77b27e
Jacoblin <sles4511@gmail.com>
2023-12-06 18:03:56 +0800

    feat: add q133

--- Q133_swap_nodes_in_pairs/Quest_test.go
+++ Q133_swap_nodes_in_pairs/Quest_test.go
@@ -21,3 +21,42 @@ func Test1(t *testing.T) {
 		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
 	}
 }
+
+func Test2(t *testing.T) {
+
+	params := parameters{head: &ListNode{}}
+
+	expected := &ListNode{}
+
+	if testResult := swapPairs(params.head); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test3(t *testing.T) {
+
+	params := parameters{head: &ListNode{Val: 1}}
+
+	expected := &ListNode{Val: 1}
+
+	if testResult := swapPairs(params.head); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test4(t *testing.T) {
+
+	params := parameters{head: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}}
+
+	expected := &ListNode{Val: 2, Next: &ListNode{Val: 1, Next: &ListNode{Val: 3}}}
+
+	if testResult := swapPairs(params.head); reflect.DeepEqual(expected, testResult) {
+		t.Log("sucess")
+	} else {
+		t.Error("fail coz expectec is ", expected, " and test result is ", testResult)
+	}
+}
--- Q133_swap_nodes_in_pairs/main.go
+++ Q133_swap_nodes_in_pairs/main.go
@@ -6,5 +6,63 @@ type ListNode struct {
 }
 
 func swapPairs(head *ListNode) *ListNode {
-	return nil
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	head, point := head.Next, head
+
+	for point != nil && point.Next != nil {
+		tmp := new(ListNode)
+		tmp.Val = point.Val
+		tmp.Next = point.Next.Next
+		point = point.Next
+		point.Next = tmp
+
+		point = point.Next.Next
+	}
+
+	return head
+}
+
+func swapPairs3(head *ListNode) *ListNode {
+	head, point := head.Next, head
+	var prev *ListNode
+
+	for point != nil && point.Next != nil {
+		tmp := point.Next
+
+		point.Next, tmp.Next = tmp.Next, point
+
+		if prev != nil {
+			prev.Next = tmp
+		}
+
+		prev, point = point, point.Next
+	}
+
+	return head
+}
+
+func swapPairs2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	point := head
+
+	for point != nil {
+		if point.Next != nil {
+			tmp := point.Val
+			point.Val = point.Next.Val
+			point.Next.Val = tmp
+			point = point.Next
+		}
+
+		if point != nil {
+			point = point.Next
+		}
+	}
+
+	return head
 }
