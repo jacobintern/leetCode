5c192c14d5a6a41f6ae6715191630415c646e740
Jacob <jacob.lin@framy.co>
2025-01-03 10:23:11 +0800

    Q149

--- Q149_longest_continuous_increasing_subsequence/main.go
+++ Q149_longest_continuous_increasing_subsequence/main.go
@@ -1,17 +1,19 @@
 package Q149
 
 func findLengthOfLCIS(nums []int) int {
-	maxC, curC, curNum := 0, 0, 0
+	maxC, curC, curNum := 0, 1, nums[0]
 
 	for _, num := range nums {
 		if num > curNum {
 			curC++
-			if curC > maxC {
-				maxC = curC
-			}
 		} else {
 			curC = 1
 		}
+
+		if curC > maxC {
+			maxC = curC
+		}
+
 		curNum = num
 	}
 
