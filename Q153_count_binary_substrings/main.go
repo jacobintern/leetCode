9ccc6cfe0f700da8c19f8564fdffdffa93a93af3
Jacob <jacob.lin@framy.co>
2025-02-11 17:28:45 +0800

    feat: add Q153

--- Q153_count_binary_substrings/main.go
+++ Q153_count_binary_substrings/main.go
@@ -1,5 +1,24 @@
 package Q153
 
 func countBinarySubstrings(s string) int {
-	return 0
+	prev, curr, count := 0, 1, 0
+
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			curr++
+		} else {
+			count += min(prev, curr)
+			prev, curr = curr, 1
+		}
+	}
+	count += min(prev, curr)
+
+	return count
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
