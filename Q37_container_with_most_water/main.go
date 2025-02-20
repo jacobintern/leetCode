6c43608653af89108802a0c534deefde0b7a63cc
Jacob <jacob.lin@framy.co>
2025-02-20 17:29:51 +0800

    feat: ...

--- Q37_container_with_most_water/main.go
+++ Q37_container_with_most_water/main.go
@@ -1,26 +1,5 @@
 package Q37
 
-// func maxArea(height []int) int {
-// 	max := 0
-// 	for i, h1 := range height {
-
-// 		for j, h2 := range height {
-// 			if i == j || i < j {
-// 				continue
-// 			}
-// 			if h1 > h2 {
-// 				max = compareArea(h2*(i-j), max)
-// 				// fmt.Println("i is ", i, ", j is ", j, ", h1 is ", h1, ", h2 is", h2, "curArea is ", h2*(i-j))
-// 			} else {
-// 				max = compareArea(h1*(i-j), max)
-// 				// fmt.Println("i is ", i, ", j is ", j, ", h1 is ", h1, ", h2 is", h2, "curArea is ", h1*(i-j))
-// 			}
-// 		}
-// 	}
-
-// 	return max
-// }
-
 func maxArea(height []int) int {
 	max := 0
 	i := 0
