8bc4b706b630ca6bdaa1d8123e666ca954646f15
Jacoblin <sles4511@gmail.com>
2025-02-27 21:04:07 +0800

    Q162

--- Q13_maximum-subarray/main.go
+++ Q13_maximum-subarray/main.go
@@ -17,3 +17,18 @@ func maxSubArray(nums []int) int {
 	}
 	return curr_max
 }
+
+func maxSubArray2(nums []int) int {
+	max, sum := nums[0], nums[0]
+	for _, v := range nums[1:] {
+		if sum < 0 {
+			sum = v
+		} else {
+			sum += v
+		}
+		if max < sum {
+			max = sum
+		}
+	}
+	return max
+}
