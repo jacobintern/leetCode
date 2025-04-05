9174ad63d63d174ac3964d249bce2397a7adf597
Jacoblin <sles4511@gmail.com>
2025-04-05 19:54:07 +0800

    Q213

---
+++ Q213_monotonic_array/Quest_test.go
@@ -0,0 +1,49 @@
+package Q213
+
+import (
+	"reflect"
+	"testing"
+)
+
+type parameters struct {
+	nums []int
+}
+
+func Test1(t *testing.T) {
+
+	params := parameters{nums: []int{1, 2, 2, 3}}
+
+	expected := true
+
+	if testResult := isMonotonic(params.nums); reflect.DeepEqual(expected, testResult) {
+		t.Log("success")
+	} else {
+		t.Error("fail coz expected is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test2(t *testing.T) {
+
+	params := parameters{nums: []int{6,5,4,4}}
+
+	expected := true
+
+	if testResult := isMonotonic(params.nums); reflect.DeepEqual(expected, testResult) {
+		t.Log("success")
+	} else {
+		t.Error("fail coz expected is ", expected, " and test result is ", testResult)
+	}
+}
+
+func Test3(t *testing.T) {
+
+	params := parameters{nums: []int{1,3,2}}
+
+	expected := false
+
+	if testResult := isMonotonic(params.nums); reflect.DeepEqual(expected, testResult) {
+		t.Log("success")
+	} else {
+		t.Error("fail coz expected is ", expected, " and test result is ", testResult)
+	}
+}
---
+++ Q213_monotonic_array/main.go
@@ -0,0 +1,16 @@
+package Q213
+
+func isMonotonic(nums []int) bool {
+	inc, dec := true, true
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			dec = false
+		}
+		if nums[i] < nums[i-1] {
+			inc = false
+		}
+	}
+
+	return inc || dec
+}
